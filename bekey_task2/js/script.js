71d887a959676d81b975130e70d210778c799cb8
Vadym <vadim.stebakov.94@gmail.com>
2018-10-20 13:25:16 +0300

    fix script.js

--- bekey_task2/js/script.js
+++ bekey_task2/js/script.js
@@ -1,14 +1,29 @@
 $(document).ready(function(){
+	var video = document.getElementById('video');
+	var img = $('.img-bred, .img-hat');
+	var imgIndex = $('#img-gift');
+	
+	video.addEventListener('timeupdate', function() {
+		var timeVideo = video.currentTime;
 
-    $('.img-bred') .delay(1000).fadeIn();
-    
-    $('.img-hat') .delay(1000).fadeIn();
+		if (timeVideo >= 1) {
+			img.fadeIn();
+		}
+		if (timeVideo >= 23 && timeVideo <= 26) {
+			imgIndex.fadeIn();
+		}
+		if (timeVideo <= 23 || timeVideo >= 26) {
+			imgIndex.fadeOut();
+		}
+	}, false);
 
-    $('#img-gift').delay(24000).fadeIn();
-
-    $('#img-gift').delay(3000).fadeOut();
-
-    $('#img-gift').click(function(){
+	imgIndex.on('click', function(){
+		if (video.paused) {
+            video.play();
+        } else {
+			video.pause();
+		}  
+		
         $('.popup').toggleClass('active');
         $('.popup').slideToggle(150)
     });
@@ -16,16 +31,8 @@ $(document).ready(function(){
     $('.popup__close').click(function(){
         $('.popup').removeClass('active');
         $('.popup').slideToggle(150);
-        $("#video").get(0).play();
+        video.play();
     });
-
-    $('#img-gift').click(function(){
-        if ($("#video").get(0).paused) {
-            $("#video").get(0).play();
-        } else {
-            $("#video").get(0).pause();
-        }  
-        
-    });  
+	
 });
 
